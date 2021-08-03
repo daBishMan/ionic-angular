a9215f22c33477617abdae916cb02486714b01b9
dabishman <jaybishtawi@outlook.com>
2021-08-02 22:06:57 -0600

    add empty pages to project, adjust routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,19 @@ import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-    // {
-    //   path: 'home',
-    //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-    // },
-    // {
-    //   path: '',
-    //   redirectTo: 'home',
-    //   pathMatch: 'full'
-    // },
-    {},
+    {path: '', pathMatch: 'full', redirectTo: 'places'},
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then((m) => m.AuthPageModule),
+    },
+    {
+        path: 'places',
+        loadChildren: () => import('./places/places.module').then((m) => m.PlacesPageModule),
+    },
+    {
+        path: 'bookings',
+        loadChildren: () => import('./bookings/bookings.module').then((m) => m.BookingsPageModule),
+    },
 ];
 
 @NgModule({
