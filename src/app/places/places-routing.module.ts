2a8c38e3c8483e39ef68adba6a3dc24262a9531e
dabishman <jaybishtawi@outlook.com>
2021-08-03 20:26:54 -0600

    added tabs and fixed routing

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -1,25 +1,72 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { PlacesPage } from './places.page';
+import {PlacesPage} from './places.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PlacesPage
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
-  }
+    {
+        // tabs is added in the redirect on an empty path, navigate to places only
+        path: 'tabs',
+        component: PlacesPage,
+        children: [
+            {
+                path: 'discover',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('./discover/discover.module').then((m) => m.DiscoverPageModule),
+                    },
+                    {
+                        path: ':placeId',
+                        loadChildren: () =>
+                            import('./discover/place-detail/place-detail.module').then((m) => m.PlaceDetailPageModule),
+                    },
+                ],
+            },
+            {
+                path: 'offers',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('./offers/offers.module').then((m) => m.OffersPageModule),
+                    },
+                    {
+                        path: 'new',
+                        loadChildren: () =>
+                            import('./offers/new-offer/new-offer.module').then((m) => m.NewOfferPageModule),
+                    },
+                    {
+                        path: 'edit/:placeId',
+                        loadChildren: () =>
+                            import('./offers/edit-offer/edit-offer.module').then((m) => m.EditOfferPageModule),
+                    },
+                    {
+                        path: ':placeId',
+                        loadChildren: () =>
+                            import('./offers/offer-bookings/offer-bookings.module').then(
+                                (m) => m.OfferBookingsPageModule
+                            ),
+                    },
+                ],
+            },
+            // this is repeated here intentionally, it can default to the discover page
+            {
+                path: '',
+                redirectTo: '/places/tabs/discover',
+                pathMatch: 'full',
+            },
+        ],
+    },
+    // You need to add this so it can default to the discover page of the tabs above
+    {
+        path: '',
+        redirectTo: '/places/tabs/discover',
+        pathMatch: 'full',
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class PlacesPageRoutingModule {}
