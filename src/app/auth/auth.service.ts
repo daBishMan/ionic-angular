0af5524d62124c6e2aa590d4cebd9b4b01e9b257
dabishman <jaybishtawi@outlook.com>
2021-08-05 11:09:52 -0600

    adding auth

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    private isAuthenticated = false;
+
+    get userIsAuthenticated() {
+        return this.isAuthenticated;
+    }
+
+    login(): void {
+        this.isAuthenticated = true;
+    }
+
+    logout(): void {
+        this.isAuthenticated = false;
+    }
+}
