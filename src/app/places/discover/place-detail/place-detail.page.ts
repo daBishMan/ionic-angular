5c0230c18b1ebf77ea745c12f546bcefff371485
dabishman <jaybishtawi@outlook.com>
2021-08-05 08:20:17 -0600

    WIP

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-place-detail',
-  templateUrl: './place-detail.page.html',
-  styleUrls: ['./place-detail.page.scss'],
+    selector: 'app-place-detail',
+    templateUrl: './place-detail.page.html',
+    styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+    constructor(private navCtrl: NavController) {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+    onBookPlace(): void {
+        // We use this instead of the angular router to ensure the proper
+        // moveForward or moveBackward animation is utilized and displayed to the user
+        this.navCtrl.navigateBack('/places/tabs/discover');
+    }
 }
